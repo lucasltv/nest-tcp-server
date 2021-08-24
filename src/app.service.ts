4c880ad4d9df38179a2ddfd6be873d215ad4d3b2
Lucas Vieira <lucas@devplace.com.br>
2021-08-23 22:42:42 -0300

    wip

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,11 +6,22 @@ export class AppService {
   private tcpServer: Server;
 
   constructor() {
-    const { PORT = 59000 } = process.env;
+    const { PORT = 1337 } = process.env;
     this.tcpServer = createServer();
 
     this.tcpServer.listen(+PORT, () =>
       console.log(`TCP SERVER STARTED - PORT: ${PORT}`),
     );
+
+    this.tcpServer.on('connection', (conn) => {
+      conn.on('data', async (data) => {
+        const rawData = data.toString();
+        console.log(`AppService ~ conn.on ~ rawData`, rawData);
+      });
+      conn.once('close', () => {
+        console.log('connection from %s closed', conn.remoteAddress);
+      });
+      conn.on('error', console.error);
+    });
   }
 }
